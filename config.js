b9736c6e55de9f75e86296202d50dc595ea6282b
Inno Irving V. Estrera <inno.irving.estrera@gmail.com>
2017-02-27 22:55:47 -0800

    Minor renaming config and indentation, and parameter to be passed to 2nd lambda

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  S3: {
+  inno: {
     accessKeyId: process.env['AWS_S3_ACCESS_KEY_ID'],
     secretAccessKey: process.env['AWS_S3_SECRET_ACCESS_KEY'],
     region: process.env['AWS_S3_REGION']
   },
-  LAMBDA: {
+  kon: {
     accessKeyId: process.env['AWS_LAMBDA_ACCESS_KEY_ID'],
       secretAccessKey: process.env['AWS_LAMBDA_SECRET_ACCESS_KEY'],
     region: process.env['AWS_LAMBDA_REGION']
--- main.js
+++ main.js
@@ -3,65 +3,66 @@
 const AWS = require('aws-sdk');
 const async = require('async');
 const schema = require('./payload-schema.json');
-const config = require('./config.js');
+const credentials = require('./config.js');
 const jsonSchemaValidator = require('jsonschema').validate;
 
-const S3 = new AWS.S3(config.S3);
-const LAMBDA = new AWS.Lambda(config.LAMBDA);
+const S3 = new AWS.S3(credentials['inno']);
+const LAMBDA = new AWS.Lambda(credentials['kon']);
 
 exports.handler = (event, context, globalCallback) => {
-    let error = validate(event);
-    if (error) return globalCallback(JSON.stringify(error), null);
+  let error = validate(event);
+  if (error) return globalCallback(JSON.stringify(error), null);
 
-    let tasks = [];
+  let tasks = [];
 
-    event.data.forEach(function(client) {
-        let id = client['id'];
-        let s3parameters = {
-            Bucket: 'decision-engine/clients',
-            Key: id.toString(),
-            ContentType: 'application/json',
-            Body: JSON.stringify(client)
-        };
+  event.data.forEach(function (client) {
+    let id = client.id;
+    let s3parameters = {
+      Bucket: 'decision-engine/clients',
+      Key: id.toString(),
+      ContentType: 'application/json',
+      Body: JSON.stringify(client)
+    };
 
-        tasks.push(function(asyncCallback) {
-            S3.putObject(s3parameters, function(err) {
-                if (err) {
-                    return asyncCallback(err, null);
-                }
-                asyncCallback(null, client);
-            });
-        });
-    });
-
-    async.parallel(tasks, function(err, results) {
+    tasks.push(function (asyncCallback) {
+      S3.putObject(s3parameters, function (err) {
         if (err) {
-            globalCallback(err, null);
-        } else {
-            const response = {
-                statusCode: 200,
-                message: 'OK. Clients created',
-                data: results
-            };
-
-            let lambdaParameters = {
-                FunctionName: 'helloworld',
-                InvocationType: 'RequestResponse',
-                Payload: JSON.stringify(response)
-            };
-            LAMBDA.invoke(lambdaParameters, function(err, data) {
-              globalCallback(err, response);
-            });
+          return asyncCallback(err, null);
         }
+        asyncCallback(null, client);
+      });
     });
+  });
 
-    function validate(event) {
-        let validationResult = jsonSchemaValidator(event, schema);
+  async.parallel(tasks, function (err, results) {
+    if (err) {
+      globalCallback(err, null);
+    } else {
+      const response = {
+        statusCode: 200,
+        message: 'OK. Clients created',
+        data: results
+      };
 
-        return validationResult.errors.length ? {
-            statusCode: 400,
-            message: validationResult.errors[0].stack,
-            data: event
-        } : null;
+      let lambdaParameters = {
+        FunctionName: 'helloworld',
+        InvocationType: 'RequestResponse',
+        Payload: JSON.stringify(results)
+      };
+      LAMBDA.invoke(lambdaParameters, function (err, data) {
+        console.log('====>', err);
+        globalCallback(err, response);
+      });
     }
+  });
+
+  function validate(event) {
+    let validationResult = jsonSchemaValidator(event, schema);
+
+    return validationResult.errors.length ? {
+        statusCode: 400,
+        message: validationResult.errors[0].stack,
+        data: event
+      } : null;
+  }
 };
